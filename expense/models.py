d3635c2db37829e18e6de5d5a17f897b1952489f
Davi Diorio Mendes <ddioriomendes@gmail.com>
2017-08-19 13:54:09 -0300

    Typo and app order.
    * Fixed typo in expense models.
    * Load admin app for last as it does not load expense app.

--- expense/models.py
+++ expense/models.py
@@ -5,12 +5,12 @@ from user.models import User
 
 class Budget(models.Model):
 
-    requestester = models.ForeignKey(User)
+    requester = models.ForeignKey(User)
     value = models.FloatField()
     description = models.TextField()
 
 
 class Reimburse(models.Model):
 
-    requestester = models.ForeignKey(User)
+    requester = models.ForeignKey(User)
     budget = models.ForeignKey(Budget)
