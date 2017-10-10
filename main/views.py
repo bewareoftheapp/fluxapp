159bdd389b12800b92e7d01bbe5f2ce4505faeec
Davi Diorio Mendes <ddioriomendes@gmail.com>
2017-10-10 19:13:26 -0300

    Correctly redirect user after login.
    Was not delegating redirection when submiting form.
    
    Fix issue #20
    
    Test Case 1:
    1. request /login/?next=/financeiro/novo-reembolso/
    2. Use a valid user-password login.
    3. Ensure was redirected to new reimburse page after login.
    
    Test Case 2:
    1. Request login page with redirection to a valid URL not registered to URLconf (e.g.: /login/?next=/xablau/)
    2. Use valid user-password login.
    3. Ensure was redirected to not found page.
    
    Test Case 3:
    1. Request login page with redirection to a invalid URL (e.g.: /login/?next=xablau)
    2. Use valid user-password login.
    3. Ensure was redirected to server error page.
    
    Also check that if user-password is not valid the error action takes user to the login page.

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@
 
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.core.urlresolvers import reverse
 
 from expense.models import Budget, Reimburse
 
@@ -19,3 +20,23 @@ def index(request):
         data['pending_budget_count'] = budgets.count()
         data['pending_reimburse_count'] = reimburse.count()
     return render(request, 'index.html', data)
+
+
+def handler404(request):
+    '''Handle not found errors.'''
+    data = {
+        'err_msg': 'Não consegui encontrar a página que você quer :(',
+        'btn_msg': 'Voltar para a página inicial',
+        'btn_href': reverse('index')
+    }
+    return render(request, 'oops.html', data)
+
+
+def handler500(request):
+    '''Handle generic server errors.'''
+    data = {
+        'err_msg': 'Não consegui fazer o que você me pediu :(',
+        'btn_msg': 'Voltar para a página inicial',
+        'btn_href': reverse('index')
+    }
+    return render(request, 'oops.html', data)
