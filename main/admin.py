b70019a5c43abc9026c9d6970ddf6055aa75a6ab
Davi Diorio Mendes <ddioriomendes@gmail.com>
2017-10-13 21:13:29 -0300

    Department models added to admin site.

--- main/admin.py
+++ main/admin.py
@@ -5,9 +5,10 @@ import user.models as UserModels
 
 from django.contrib import admin
 
+import department.models as DepartmentModels
 import expense.models as ExpenseModels
 
-
 admin.site.register(ExpenseModels.Reimburse)
-admin.site.register(UserModels.Department)
+admin.site.register(DepartmentModels.Department)
+admin.site.register(DepartmentModels.UserDepartmentRelation)
 admin.site.register(UserModels.RegistrationToken)
